25a6d94a9d227fdff9bef6d162c0f8bd79ae484f
Petar Jolakoski <74726856+pero-jolak@users.noreply.github.com>
2024-09-24 15:29:24 +0200

    Add files via upload

--- RGBM with resetting.py
+++ RGBM with resetting.py
@@ -14,11 +14,13 @@ def rgbm(x0, N, mu, sigma, Tc, tau, dt, xr, r):
     x = x0
     
     for t in range(1,len(T)):
-        epsilon = np.random.randn(1,N)
+        epsilon = np.random.randn(1,N) # noise
         
-        dx = x*(mu*dt + sigma * np.dot(np.sqrt(dt), epsilon.T)) - tau * (x - np.mean(x)) * dt
+        dx = x*(mu*dt + sigma * np.dot(np.sqrt(dt), epsilon.T)) - tau * (x - np.mean(x)) * dt  # RGBM part
         x = x + dx
-                            
+        
+        # Reset random trajectories with probability r*dt
+                    
         x_r = np.random.binomial(1, r*dt, N)
         
         if np.argwhere(x_r).size > 0:
@@ -32,21 +34,23 @@ def rgbm(x0, N, mu, sigma, Tc, tau, dt, xr, r):
 
 #%% Simulate trajectories
 
-N = 100
-x_0 = 1
+N = 100 # number of trajectories
+x_0 = 1 # initial position
 
 x0 = np.ones(N).reshape(N,1) * x_0
 
-mu = 0.021
-sigma = np.sqrt(0.01)
-Tc = 100
-tau = -0.01
-xr = x_0
-r = 0
-dt = 0.1
+mu = 0.021 # drift
+sigma = np.sqrt(0.01) # noise amplitude
+Tc = 100 # time
+tau = -0.01 # reallocation parameter
+xr = x_0 # resetting position
+r = 0 # resetting rate
+dt = 0.1 # time interval
 
 X_Vec,T = rgbm(x0, N, mu, sigma, Tc, tau, dt, xr, r)
 
 #%% Plot
 
 plt.plot(T, X_Vec)
+plt.xlabel('$t$')
+plt.ylabel('$x(t)$')
